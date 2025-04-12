5a18b611c38f05e870fa130df26fc5b628e40f6c
Diego Martinez <diego.martinez@vallcompanys.es>
2025-04-12 16:39:19 +0200

    Register and login/logout views created

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # login automático tras registrarse
+            return redirect('home')  # puedes cambiar esta redirección
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
